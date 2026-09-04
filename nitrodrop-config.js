c02af95cb87f9b25f6373bd75f51a1fcc61c15ca
nkstv <lescagoules05@gmail.com>
2026-09-04 17:54:14 +0200

    Mise à jour de la config NitroDrop depuis le panel admin

--- nitrodrop-config.js
+++ nitrodrop-config.js
@@ -251,6 +251,27 @@ const NITRODROP_DEFAULT_CONFIG = {
           "signupHighlighted": true,
           "claimHighlighted": false,
           "depositRequired": false
+        },
+        {
+          "featured": false,
+          "badge": "",
+          "mark": "W",
+          "markColorStart": "#198dc8",
+          "markColorEnd": "#57c5db",
+          "markClass": "mark-1",
+          "name": "Winna",
+          "sub": "Crypto Casino · Best Deals",
+          "bonusAmount": "Free Money",
+          "bonusSuffix": "one time",
+          "desc": "Register with the link, submit your username, and get your tip. **No deposit** needed. ",
+          "code": "luckymood",
+          "codeEnabled": true,
+          "depositRequired": false,
+          "signupHref": "https://winna.com/?r=luckymood",
+          "signupText": "1. Create Account",
+          "claimText": "2. Claim My Bonus",
+          "signupHighlighted": true,
+          "claimHighlighted": false
         }
       ],
       "tickerSettings": {
